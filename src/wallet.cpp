223bab9b1501ed4a5ccb52ecd56c500a88cd0821
BlueDragon747 <bluedragon747@gmail.com>
2015-08-12 21:38:36 +0100

    Update wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -2,6 +2,7 @@
 // Copyright (c) 2009-2014 The Bitcoin developers
 // Copyright (c) 2013-2015 The Blakecoin developers
 // Copyright (c) 2013-2015 The Photon developers
+// Copyright (C) 2014-2015 The BlakeBitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
